8184a3d0a081a9cbb1aa0bc4303f91b47029f388
William Wang <61264396+william-ying@users.noreply.github.com>
2025-11-06 21:08:00 -0800

    Update bst.h

--- bst.h
+++ bst.h
@@ -345,7 +345,7 @@ BinarySearchTree<Key, Value>::iterator::operator++()
     while (current_->getRight() == NULL) {
         Node<Key, Value>* temp = current_;
         current_ = current_->getParent();
-        if (current_->getLeft() == temp) {
+        if (current_->getLeft() != NULL && current_->getLeft() == temp) {
             cout << current_->getKey() << " l " << current_->getValue() << endl;
             return *this;
         }
