baaf3e84665e8b0b031ea1d568a320fb26da79a7
William Wang <61264396+william-ying@users.noreply.github.com>
2025-11-07 01:02:24 -0800

    Update avlbst.h

--- avlbst.h
+++ avlbst.h
@@ -137,7 +137,7 @@ public:
 protected:
     virtual void nodeSwap( AVLNode<Key,Value>* n1, AVLNode<Key,Value>* n2);
     // Add helper functions here
-    AVLNode<Key, Value>* override root_;
+    AVLNode<Key, Value>* root_ override;
 
 
 };
