0e48dcdb01f81290c6c47fa5ba4cc1cbdafd3268
William Wang <61264396+william-ying@users.noreply.github.com>
2025-11-06 20:28:10 -0800

    Update bst-test.cpp

--- bst-test.cpp
+++ bst-test.cpp
@@ -14,6 +14,7 @@ int main(int argc, char *argv[])
     bt.insert(std::make_pair('b',2));
     
     cout << "Binary Search Tree contents:" << endl;
+    cout << bt.begin()->first << endl;
     for(BinarySearchTree<char,int>::iterator it = bt.begin(); it != bt.end(); ++it) {
         cout << it->first << " " << it->second << endl;
     }
