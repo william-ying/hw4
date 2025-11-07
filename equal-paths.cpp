290e4557fc6547b7c5032a826972e959e03146f6
William Wang <61264396+william-ying@users.noreply.github.com>
2025-11-07 01:55:01 -0800

    Update equal-paths.cpp

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -20,8 +20,13 @@ bool equalPaths(Node * root)
 
 int depth(Node * root) {
     if (root->left == NULL && root->right == NULL) return 0;
-    else if (root->right == NULL) return depth(root->left) + 1;
-    else if (root->left == NULL) return depth(root->right + 1);
+    else if (root->right == NULL) {
+        if (depth(root->left) != -1) return depth(root->left) + 1;
+        else {return -1;}
+    } else if (root->left == NULL) {
+        if (depth(root->left) != -1) return depth(root->right + 1);
+        else {return -1;}
+    }
     else if (depth(root->left) == -1 || depth(root->right) == -1) return -1;
     else if (depth(root->left) == depth(root->right)) {return depth(root->left) + 1;}
     else {
