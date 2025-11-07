3af067eb864ace9d8a3657d5dea4534f7e39e9e7
William Wang <61264396+william-ying@users.noreply.github.com>
2025-11-07 14:34:00 -0800

    Update bst-test.cpp

--- bst-test.cpp
+++ bst-test.cpp
@@ -24,23 +24,23 @@ int main(int argc, char *argv[])
     cout << "Erasing b" << endl;
     bt.remove(100);
 
-    // AVL Tree Tests
-    // AVLTree<char,int> at;
-    // at.insert(std::make_pair('a',1));
-    // at.insert(std::make_pair('b',2));
+    AVL Tree Tests
+    AVLTree<char,int> at;
+    at.insert(std::make_pair('a',1));
+    at.insert(std::make_pair('b',2));
 
-    // cout << "\nAVLTree contents:" << endl;
-    // for(AVLTree<char,int>::iterator it = at.begin(); it != at.end(); ++it) {
-    //     cout << it->first << " " << it->second << endl;
-    // }
-    // if(at.find('b') != at.end()) {
-    //     cout << "Found b" << endl;
-    // }
-    // else {
-    //     cout << "Did not find b" << endl;
-    // }
-    // cout << "Erasing b" << endl;
-    // at.remove('b');
+    cout << "\nAVLTree contents:" << endl;
+    for(AVLTree<char,int>::iterator it = at.begin(); it != at.end(); ++it) {
+        cout << it->first << " " << it->second << endl;
+    }
+    if(at.find('b') != at.end()) {
+        cout << "Found b" << endl;
+    }
+    else {
+        cout << "Did not find b" << endl;
+    }
+    cout << "Erasing b" << endl;
+    at.remove('b');
 
     return 0;
 }
