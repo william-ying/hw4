2ffb4a304cda587ca53beedef49dcd30dc45c923
William Wang <61264396+william-ying@users.noreply.github.com>
2025-11-07 22:39:36 -0800

    Update avlbst.h

--- avlbst.h
+++ avlbst.h
@@ -225,10 +225,10 @@ void AVLTree<Key, Value>::insert (const std::pair<const Key, Value> &new_item)
                     // cout << currt->getLeft()->getKey() << endl;
                     // cout << currt->getLeft()->getBalance() << endl;
                     AVLNode<Key, Value>* temp = currt->getLeft();
-                    (if currt2->getLeft() != NULL) currt2->getLeft()->setParent(currt);
+                    if (currt2->getLeft() != NULL) currt2->getLeft()->setParent(currt);
                     currt->setLeft(currt2->getLeft());
                     currt2->setLeft(currt2->getRight());
-                    (if temp != NULL) temp->setParent(currt2);
+                    if (temp != NULL) temp->setParent(currt2);
                     currt2->setRight(temp);
                 }
                 AVLNode<Key, Value>* temp = currt->getLeft();
@@ -284,10 +284,10 @@ void AVLTree<Key, Value>::insert (const std::pair<const Key, Value> &new_item)
                     
                 if (currt->getRight() != NULL) {
                     AVLNode<Key, Value>* temp = currt->getRight();
-                    (if currt2->getRight() != NULL) currt2->getRight()->setParent(currt);
+                    if (currt2->getRight() != NULL) currt2->getRight()->setParent(currt);
                     currt->setRight(currt2->getRight());
                     currt2->setRight(currt2->getLeft());
-                    (if temp != NULL) temp->setParent(currt2);
+                    if (temp != NULL) temp->setParent(currt2);
                     currt2->setLeft(temp);
                 }
                 AVLNode<Key, Value>* temp = currt->getRight();
